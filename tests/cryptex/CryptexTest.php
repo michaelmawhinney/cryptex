e97d3e78f737b04ba59f8936f2a9cda562335f78
Michael Mawhinney <michael.mawhinney.jr@gmail.com>
2023-08-04 16:17:31 +0000

    Add basic PHPunit testing

---
+++ tests/cryptex/CryptexTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace cryptex;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CryptexTest
+ *
+ * This class is used to perform unit testing for the Cryptex class.
+ *
+ * @package Cryptex
+ */
+final class CryptexTest extends TestCase
+{
+    private $key;
+    private $salt;
+    private $plaintext;
+    private $ciphertext;
+
+    protected function setUp(): void
+    {
+        $this->key = '1-2-3-4-5';
+        $this->salt = Cryptex::generateSalt();
+        $this->plaintext = "You're a certified prince.";
+        $this->ciphertext = Cryptex::encrypt($this->plaintext, $this->key, $this->salt);
+    }
+
+    public function testEncryptDecrypt(): void
+    {
+        $this->assertIsString($this->ciphertext);
+        $this->assertNotEquals($this->plaintext, $this->ciphertext);
+
+        $decrypted = Cryptex::decrypt($this->ciphertext, $this->key, $this->salt);
+        $this->assertEquals($this->plaintext, $decrypted);
+    }
+}
