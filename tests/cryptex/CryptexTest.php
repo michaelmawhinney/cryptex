6dd514f4c5b30c63fdef10342932c985b0ff0635
Michael Mawhinney <michael.mawhinney.jr@gmail.com>
2023-08-04 16:33:58 +0000

    Update unit testing for generateSalt()

--- tests/cryptex/CryptexTest.php
+++ tests/cryptex/CryptexTest.php
@@ -3,6 +3,7 @@
 namespace cryptex;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 /**
  * Class CryptexTest
@@ -15,11 +16,17 @@ final class CryptexTest extends TestCase
 {
     private $key;
     private $salt;
+    private $saltLength;
     private $plaintext;
     private $ciphertext;
 
     protected function setUp(): void
     {
+        // Access the private vars with reflection
+        $reflection = new ReflectionClass(Cryptex::class);
+        $this->saltLength = $reflection->getReflectionConstant('SALT_LENGTH')->getValue();
+
+        // Set the remaining vars
         $this->key = '1-2-3-4-5';
         $this->salt = Cryptex::generateSalt();
         $this->plaintext = "You're a certified prince.";
@@ -34,4 +41,12 @@ final class CryptexTest extends TestCase
         $decrypted = Cryptex::decrypt($this->ciphertext, $this->key, $this->salt);
         $this->assertEquals($this->plaintext, $decrypted);
     }
+
+    public function testGenerateSalt(): void
+    {
+        $this->assertIsInt($this->saltLength);
+        $this->assertIsString($this->salt);
+        $this->assertEquals($this->saltLength, strlen($this->salt));
+    }
+
 }
